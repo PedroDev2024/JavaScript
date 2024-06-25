0a8f43ed095a24be22dd14fbf92b5f83e018c9ed
PedroDev2024 <pedro.lg2017@gmail.com>
2024-06-24 23:02:24 -0300

    d

---
+++ curso-em-video/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //Condção simples!
+    console.log(`Voce ultrapassou o limite. Multado!!!`)
+}
+console.log(`Dirija sempre usando cito de segurança.`)
