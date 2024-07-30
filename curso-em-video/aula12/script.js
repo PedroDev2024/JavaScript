0fb40bcec3afaf53f92d50ad6bf81b175f2db5a4
PedroDev2024 <pedro.lg2017@gmail.com>
2024-07-30 18:44:10 -0300

    k

--- curso-em-video/aula12/script.js
+++ curso-em-video/aula12/script.js
@@ -1,1 +1,18 @@
-alert('Ola!')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+// var hora = data.getHours()
+var hora = 3
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 5 && hora <12){
+    //Bom Dia!!
+    img.src = 'img/imagemmanha.jpg'
+} else if (hora >= 12 && hora <18) {
+    //Boa tarde
+    img.src = 'img/imagemtarde.jpg'
+} else {
+    //Boa noite
+    img.src = 'img/imagemnoite.jpg'
+}
+}
