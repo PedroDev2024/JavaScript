a7613ee565a69afc82562d6a41420a7c0d5248fe
PedroDev2024 <pedro.lg2017@gmail.com>
2024-09-13 06:56:36 -0300

    s

---
+++ curso-em-video/aula13b/ambiente.js
@@ -0,0 +1,22 @@
+var C = 1
+do {
+    console.log(`Passo ${C}`)
+    C++
+} while (C <=10)
+
+/*var C = 1
+while (C <= 6){
+    console.log(`Passo ${C}`)
+    C++
+}
+*/
+
+/*
+console.log('Tudobem')
+console.log('Tudobem')
+console.log('Tudobem')
+console.log('Tudobem')
+console.log('Tudobem')
+console.log('Tudobem')
+*/
+
